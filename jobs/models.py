a9f0e2e7eb9b3310abce40c2b27f9e78c077cda2
Kelechi David Igbokwe <kelechi_igbokwe@yahoo.com>
2018-05-15 18:34:43 +0200

    added static MEDIA_URL and ROOT

--- jobs/models.py
+++ jobs/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Jobs(models.Model):
-    image = models.ImageField(upload_to='')#image field to be uploaded by user for the job section
+    image = models.ImageField(upload_to='images')#image field to be uploaded by user for the job section
     summary = models.CharField(max_length=200) #summary of the job
