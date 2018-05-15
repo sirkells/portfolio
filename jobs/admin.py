0fe3937973ed68045147460ed54cd0389df63134
Kelechi David Igbokwe <kelechi_igbokwe@yahoo.com>
2018-05-15 18:19:32 +0200

    Added jobs to the admin page and created superuser account

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Jobs #importing the Job class created in models
+admin.site.register(Jobs)
--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Jobs(models.Model):
+    image = models.ImageField(upload_to='')#image field to be uploaded by user for the job section
+    summary = models.CharField(max_length=200) #summary of the job
