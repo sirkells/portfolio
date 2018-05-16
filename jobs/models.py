fba1811746ba8775ae0de4c33da003c4879ab1e9
Kelechi David Igbokwe <kelechi_igbokwe@yahoo.com>
2018-05-16 08:59:25 +0200

    Added Bootstrap and Jobs are viewable on homepage

--- jobs/models.py
+++ jobs/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Job(models.Model):
-    image = models.ImageField(upload_to='images')#image field to be uploaded by user for the job section
+    image = models.ImageField(upload_to='images/')#image field to be uploaded by user for the job section
     summary = models.CharField(max_length=200) #summary of the job
